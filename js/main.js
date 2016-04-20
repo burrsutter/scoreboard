7edbf3dc2672f1c86ee6286c313a612057f5ab16
joshuawilson <javajoshw@gmail.com>
2016-04-19 23:41:47 -0400

    Initial attempt at configuring to run on just NPM and Browserify.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 'use strict'
 
-import 'babel-polyfill';
-import world from './world';
-require("../styles/main.css");
-
-document.getElementById('output').innerHTML = `Hello ${world}!`;
-
 var transform = ["transform", "msTransform", "webkitTransform", "mozTransform", "oTransform"];
+var team_1_item = document.getElementById('team-1');
+var team_2_item = document.getElementById('team-2');
+var team_3_item = document.getElementById('team-3');
+var team_4_item = document.getElementById('team-4');
+var transformProperty = getSupportedPropertyName(transform);
+var valueX = 0;
+var valueY = 0;
+var teams = ['team-1', 'team-2', 'team-3', 'team-4'];
+var teamItems = [
+  team_1_item,
+  team_2_item,
+  team_3_item,
+  team_4_item
+];
+
 
 function getSupportedPropertyName(properties) {
   for (var i = 0; i < properties.length; i++) {
@@ -17,33 +26,18 @@ function getSupportedPropertyName(properties) {
   return null;
 }
 
-var team_1_item = document.getElementById('team-1');
-var team_2_item = document.getElementById('team-2');
-var team_3_item = document.getElementById('team-3');
-var team_4_item = document.getElementById('team-4');
-var transformProperty = getSupportedPropertyName(transform);
-let valueX = 0;
-let valueY = 0;
 
 function addRandomNumTo(x) {
-  let random = Math.ceil(Math.random() * 5); // return a random number between 1 and 10
+  var random = Math.ceil(Math.random() * 5); // return a random number between 1 and 10
   return x + random;
 }
-
-let teams = ['team-1', 'team-2', 'team-3', 'team-4'];
-let teamItems = [
-  team_1_item,
-  team_2_item,
-  team_3_item,
-  team_4_item
-];
 
 
 function upPoints() {
   valueY = addRandomNumTo(valueY);
-  let teamItemSelector = Math.floor(Math.random() * 4); // 0-3
-  let x = valueX + 'px';
-  let y = valueY + 'px';
+  var teamItemSelector = Math.floor(Math.random() * 4); // 0-3
+  var x = valueX + 'px';
+  var y = valueY + 'px';
   if (transformProperty) {
     teamItems[teamItemSelector].style[transformProperty] = `translate3d(${x}, ${y}, 0)`;
   }
