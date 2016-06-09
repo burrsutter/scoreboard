6071fc76fd6f1dbcf71006a939cdd8ad0424071a
Burr Sutter <bsutter@redhat.com>
2016-06-09 16:30:06 -0400

    added websocket logic to receive scores

--- js/getScores.js
+++ js/getScores.js
@@ -14,10 +14,32 @@ module.exports = function (scores, racerPositions) {
   //   if (xhr.readyState == 4) {
   //     if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
   //       console.log("xhr succeeded: " + xhr.status + ' results: ' + xhr.responseText);
-        scores = scoreGen(scores);
+        // scores = scoreGen(scores);
+        // console.log("HERE");
+        var scores = new Array(4);
+        scores[0] = 1;
+        scores[1] = 1;
+        scores[2] = 1;
+        scores[3] = 1;
         
-        // TODO: move to within xhr
-        processScores(scores, racerPositions);
+        // declare websocket
+        var ws = new WebSocket('ws://localhost:9001/scoreboard');
+        
+        ws.onopen = event => {
+          // console.log(event);
+        };
+
+        ws.onmessage = event => {
+          console.log(event);
+          let message = JSON.parse(event.data);
+          
+          scores[0] = message[0].score;          
+          scores[1] = message[1].score;
+          scores[2] = message[2].score;
+          scores[3] = message[3].score;
+          
+          processScores(scores, racerPositions);
+        };
         
         return scores;
   //     } else {
